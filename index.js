f8922edf52477a15f0a2dbdfa349c97acb5ca5e8
Vladislav Golovin <vladislav.golovin95@mail.ru>
2023-05-29 21:39:09 +0300

    added post date and error handler

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const inputTitleNode = document.getElementById('inputTitlePost');
 const inputTextNode = document.getElementById('inputTextPost');
 const postsNode = document.getElementById('ribbonPosts');
 const addPostBtnNode = document.getElementById('addPostBtn');
+const validationMessagePostTitle = document.getElementById('validationMessagePostTitle');
+const validationMessagePostText = document.getElementById('validationMessagePostText');
 
 const posts = [];
 
@@ -11,9 +13,40 @@ addPostBtnNode.addEventListener('click', function() {
     renderPosts();
 });
 
+inputTitleNode.addEventListener('input', function(event) {
+    const presentValue = event.target.value;
+    const charactersTitle = presentValue.length;
+    
+    if (charactersTitle > 100) {
+        validationMessagePostTitle.className = 'validationMessagePostTitle_active';
+        addPostBtnNode.setAttribute('disabled', true);
+    } else if (charactersTitle == 0) {
+        addPostBtnNode.setAttribute('disabled', true);
+    } else {
+        validationMessagePostTitle.className = 'validationMessagePostTitle';
+        addPostBtnNode.removeAttribute('disabled');
+    }
+});
+
+inputTextNode.addEventListener('input', function(event) {
+    const presentValue = event.target.value;
+    const charactersText = presentValue.length;
+    
+    if (charactersText > 200) {
+        validationMessagePostText.className = 'validationMessagePostText_active';
+        addPostBtnNode.setAttribute('disabled', true);
+    } else if (charactersText == 0) {
+        addPostBtnNode.setAttribute('disabled', true);
+    } else {
+        validationMessagePostText.className = 'validationMessagePostText';
+        addPostBtnNode.removeAttribute('disabled');
+    }
+});
+
 function getPostFromUser() {
     const title = inputTitleNode.value;
     const text = inputTextNode.value;
+
     return {
         title,
         text
@@ -21,7 +54,15 @@ function getPostFromUser() {
 }
 
 function addPost({title, text}) {
+    const currentDate = new Date();
+    const dateTime = `${currentDate.toLocaleDateString()} ${currentDate.getHours()}:${currentDate.getMinutes()}`;
+
+    if (title, text == 0) {
+        return;
+    }
+
     posts.push({
+        dateTime,
         title: title,
         text: text
     });
@@ -37,7 +78,7 @@ function renderPosts() {
     posts.forEach(post => {
         postsHTML += `
             <div class='jsPost'>
-                <p class='jsPost__date'>${date} ${time}</p>
+                <p class='jsPost__date'>${post.dateTime}</p>
                 <p class='jsPost__title'>${post.title}</p>
                 <p class='jsPost__text'>${post.text}</p>
             </div>
@@ -45,8 +86,3 @@ function renderPosts() {
     });
     postsNode.innerHTML = postsHTML;
 }
-
-//------------Дата и время-----------------
-const currentDate = new Date();
-const date = currentDate.toLocaleDateString();
-const time = currentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
